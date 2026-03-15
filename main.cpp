988764a3334516499bfac55f995e7f4e8ca8374e
miguelgarc5605 <miguelgarc5605@gmail.com>
2026-03-15 07:34:10 -0500

    Created Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+bool didGladInitialize();
+bool isWindowNull(GLFWwindow* window);
+
+int WINDOW_WIDTH = 1000;
+int WINDOW_HEIGHT = 1000;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+
+    // Create Window
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Project Brick", NULL, NULL);
+    if (isWindowNull(window)) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!didGladInitialize()) return -1;
+
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // rendering commands
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check/call events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+bool didGladInitialize() {
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool isWindowNull(GLFWwindow* window) {
+    if (window == NULL) {
+        std::cout << "GLFW window is null" << std::endl;
+        return true;
+    }
+    return false;
 }
