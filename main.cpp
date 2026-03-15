51a468005f9be12cd1061c51256d6716b8702054
miguelgarc5605 <miguelgarc5605@gmail.com>
2026-03-15 08:11:51 -0500

    Display Single Game Pixel

--- main.cpp
+++ main.cpp
@@ -7,14 +7,41 @@ void processInput(GLFWwindow *window);
 bool didGladInitialize();
 bool isWindowNull(GLFWwindow* window);
 
-int WINDOW_WIDTH = 1000;
-int WINDOW_HEIGHT = 1000;
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+    "}\n\0";
+
+const int WINDOW_SIZE = 1000;
+const int GAME_PIXEL_SIZE = 10; // One game pixel is equal to a 10x10 pixel on screen
+const float GAME_PIXEL_FLOAT_SIZE = float(GAME_PIXEL_SIZE) / float(WINDOW_SIZE); // 0.01f
+
+const float VERTICES[] = {
+    -GAME_PIXEL_FLOAT_SIZE, -GAME_PIXEL_FLOAT_SIZE, 0.0f,  // bottom left
+    GAME_PIXEL_FLOAT_SIZE, -GAME_PIXEL_FLOAT_SIZE, 0.0f,  // bottom right
+    -GAME_PIXEL_FLOAT_SIZE,  GAME_PIXEL_FLOAT_SIZE, 0.0f,  // top left
+    GAME_PIXEL_FLOAT_SIZE,  GAME_PIXEL_FLOAT_SIZE, 0.0f  // top right
+};
+
+const unsigned int INDICES[] = {  // note that we start from 0!
+    0, 1, 2,  // first Triangle
+    1, 2, 3   // second Triangle
+};
 
 int main() {
     glfwInit();
 
     // Create Window
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Project Brick", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_SIZE, WINDOW_SIZE, "Project Brick", NULL, NULL);
     if (isWindowNull(window)) {
         glfwTerminate();
         return -1;
@@ -23,24 +50,80 @@ int main() {
 
     if (!didGladInitialize()) return -1;
 
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glViewport(0, 0, WINDOW_SIZE, WINDOW_SIZE);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // create vertex arrays
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // create buffer
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES), VERTICES, GL_STATIC_DRAW);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES), INDICES, GL_STATIC_DRAW);
+
+    // compile vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // compile fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // create shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window)) {
-        // input
+        // INPUT
         processInput(window);
 
-        // rendering commands
+        // RENDERING
+
+        // clear screen black
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // check/call events and swap buffers
+        // Draw Pixel
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // CHECK/CALL EVENTS && SWAP BUFFERS
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void createSquare(int width, int height) {
+    // Create Vertices
+
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
